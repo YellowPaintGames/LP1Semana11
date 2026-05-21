ba650eb96d2eb80f837c301201f9e65d96583041
Francisco Caldeira <kikocaldeira2007@gmail.com>
2026-05-21 16:35:22 +0100

    Fourth Commit

--- StringGenerator/Program.cs
+++ StringGenerator/Program.cs
@@ -3,11 +3,16 @@ using System.Text;
 
 namespace StringGenerator
 {
+    /// <summary>
+    /// The place where everything happens
+    /// </summary>
     class Program
     {
         static void Main(string[] args)
         {
+            //Controller created here
             Controller C = new Controller();
+            //And using the first argument makes a string of numbers
             C.GenerateAString(args[0]);
         }
     }
