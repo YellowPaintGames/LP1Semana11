2279ae0cb1bac57f392daa879a37f9ea99a58f96
Francisco Caldeira <kikocaldeira2007@gmail.com>
2026-05-21 16:35:59 +0100

    Fifth Commit

--- StringGenerator/View.cs
+++ StringGenerator/View.cs
@@ -2,8 +2,15 @@ using System;
 using System.Text;
 namespace StringGenerator
 {
+    /// <summary>
+    /// Visual feedback for the user
+    /// </summary>
     public class View
     {
+        /// <summary>
+        /// Does a Console Write Line, lol
+        /// </summary>
+        /// <param name="output">Whatever you need to be printed out</param>
         public void PrintOut(string output)
         {
             Console.Write(output);
